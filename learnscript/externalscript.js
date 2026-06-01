f2e99377cb643b9805f6dad22ce9ab4f55e7b781
voleqnga <ngavlqk24411e@st.uel.edu.vn>
2026-06-01 17:10:29 +0700

    Learnscript

---
+++ learnscript/externalscript.js
@@ -0,0 +1,18 @@
+function showVN()
+{
+    id_div_vi.style.display = 'block';
+    id_div_kor.style.display = 'none';
+    id_div_chinese.style.display = 'none';
+}
+function showKR()
+{
+    id_div_vi.style.display = 'none';
+    id_div_kor.style.display = 'block';
+    id_div_chinese.style.display = 'none';
+}
+function showCN()
+{
+    id_div_vi.style.display = 'none';  
+    id_div_kor.style.display = 'none';
+    id_div_chinese.style.display = 'block';
+}
