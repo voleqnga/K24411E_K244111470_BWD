56be5a4ce9cf1bba7469dab3d9ea722cdb46dee1
voleqnga <ngavlqk24411e@st.uel.edu.vn>
2026-06-17 11:15:25 +0700

    Update ex67

---
+++ ex67/js/mainjs.js
@@ -0,0 +1,42 @@
+/**
+ * students_xml_text list of customer (XML -> customer.js)
+ * bodystudent ->tbody 
+ * @param {*} students_xml_text 
+ * @param {*} bodystudent
+ */
+function load_customers(students_xml_text,bodystudent)
+{
+    var parser = new DOMParser(); 
+    var xmlDoc = parser.parseFromString(students_xml_text,"text/xml"); 
+    //đọc toàn bộ thẻ customers
+    tag_students=xmlDoc.getElementsByTagName("student")
+    for (i = 0; i < tag_students.length; i++)
+    {
+        value_tag_id=tag_students[i].getElementsByTagName("id")[0].childNodes[0].nodeValue
+        value_tag_name=tag_students[i].getElementsByTagName("name")[0].childNodes[0].nodeValue
+        value_tag_bday=tag_students[i].getElementsByTagName("birthday")[0].childNodes[0].nodeValue
+        value_tag_gender=tag_students[i].getElementsByTagName("gender")[0].childNodes[0].nodeValue
+        tr=document.createElement("tr")
+        td_id=document.createElement("td")
+        td_id.innerHTML=value_tag_id
+        td_name=document.createElement("td")
+        td_name.innerHTML=value_tag_name
+        td_bday=document.createElement("td")
+        td_bday.innerHTML=value_tag_bday       
+        td_gender=document.createElement("td")
+        td_gender.innerHTML=value_tag_gender
+        tr.appendChild(td_id)
+        tr.appendChild(td_name)
+        tr.appendChild(td_bday)
+        tr.appendChild(td_gender)
+        
+        tr.onclick = function() {
+                document.getElementById("detail").style.display = "table";
+                document.getElementById("detid").textContent = this.cells[0].textContent;
+                document.getElementById("detname").textContent = this.cells[1].textContent;
+                document.getElementById("detbday").textContent = this.cells[2].textContent;
+                document.getElementById("detgender").textContent = this.cells[3].textContent;
+            };
+        bodystudent.appendChild(tr)
+    }
+}
--- learnXMLDOM/customer.js
+++ learnXMLDOM/customer.js
@@ -1,28 +1,26 @@
-var customer="<customer>" +
-    "<customer>" +
-        "<id>1</id>" +
-        "<name>John Doe</name>" +
-        "<age>30</age>" +
-        "<city>New York</city>" +
-    "</customer>" +
-
-    "<customer>" +
-        "<id>2</id>" +
-        "<name>Jane Smith</name>" +
-        "<age>25</age>" +
-        "<city>Los Angeles</city>" +
-    "</customer>" +
-
-    "<customer>" +
-        "<id>3</id>" +
-        "<name>Bob Johnson</name>" +
-        "<age>40</age>" +
-        "<city>Chicago</city>" +
-    "</customer>"
-
-    "<customer>" +
-        "<id>4</id>" +
-        "<name>Bob BTS</name>" +
-        "<age>18</age>" +
-        "<city>HCM</city>" +
-    "</customer>"
+var customers="<customers>" +
+                "<customer>" +
+                "<id>1</id>" +
+                "<name>John Doe</name>" +   
+                "<age>30</age>" +
+                "<city>New York</city>" +
+                "</customer>" +
+                "<customer>" +
+                "<id>2</id>" +
+                "<name>Jane Smith</name>" +
+                "<age>25</age>" +
+                "<city>Los Angeles</city>" +
+                "</customer>" +
+                "<customer>" +   
+                "<id>3</id>" +
+                "<name>Bob Johnson</name>" +
+                "<age>40</age>" +
+                "<city>Chicago</city>" +
+                "</customer>" +
+                "<customer>" +   
+                "<id>4</id>" +
+                "<name>Nguyễn Văn Tèo</name>" +
+                "<age>48</age>" +
+                "<city>Hồ Chí Minh</city>" +
+                "</customer>" +
+                "</customers>";
