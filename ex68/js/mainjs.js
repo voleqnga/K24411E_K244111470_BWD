1f2e8558d0dac55b239df7b0ad8a4f66f83bf33e
voleqnga <ngavlqk24411e@st.uel.edu.vn>
2026-06-17 11:44:52 +0700

    ex67

---
+++ ex68/js/mainjs.js
@@ -0,0 +1,59 @@
+function load_cd_fromxml(dataset_path,bodycd)
+{
+    var xhr =new XMLHttpRequest();
+    xhr.open("GET",dataset_path,true);
+    xhr.send();
+    xhr.onreadystatechange=function()
+    {
+        if (xhr.readyState==4 && xhr.status==200)
+        {
+            //handling when loading data successfully
+            //var xmlDoc = parser.parseFromString(xhr.responseXML,"text/xml"); 
+            var xmlDoc = xhr.responseXML;
+            
+            if (xmlDoc == null)
+            {
+                alert("Lỗi: Không thể parse file XML. Vui lòng kiểm tra cú pháp XML.");
+                return;
+            }
+            tag_cds=xmlDoc.getElementsByTagName("cd")
+            for (i = 0; i < tag_cds.length; i++)
+            {
+                value_tag_artist=tag_cd[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue
+                value_tag_title=tag_cd[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue
+                value_tag_country=tag_cd[i].getElementsByTagName("COUNTRY")[0].childNodes[0].nodeValue
+                value_tag_company=tag_cd[i].getElementsByTagName("COMPANY")[0].childNodes[0].nodeValue
+                value_tag_price=tag_cd[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue
+                value_tag_year=tag_cd[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue
+
+                tr=document.createElement("tr")
+                td_artist=document.createElement("td")
+                td_artist.innerHTML=value_tag_artist
+                td_title=document.createElement("td")
+                td_title.innerHTML=value_tag_title
+                td_country=document.createElement("td")
+                td_country.innerHTML=value_tag_country
+                td_company=document.createElement("td")
+                td_company.innerHTML=value_tag_company
+                td_price=documnet.createElement("td")
+                td_price.innerHTML=value_tag_price
+                td_year=document.createElement("td")
+                td_year.innerHTML=value_tag_year
+    
+                tr.appendChild(td_artist)
+                tr.appendChild(td_title)
+                tr.appendChild(td_country)
+                tr.appendChild(td_company)
+                tr.appendChild(td_price)
+                tr.appendChild(td_year)
+                
+                bodycd.appendChild(tr)
+            }
+        }
+        else
+        {
+            //handling when data can't be loaded
+        }
+    }
+
+}
